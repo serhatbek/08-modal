f95a7965d3373bf004329eeca8c21cc413785a52
serhatbek <serhat.bekk@gmail.com>
2022-03-02 23:56:20 +0300

    modal functionality added

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,15 @@
 if (module.hot) {
   module.hot.accept();
 }
+
+const openModalBtn = document.querySelector('.cta__btn');
+const closeModalBtn = document.querySelector('.modal__close');
+const modal = document.querySelector('.modal');
+
+openModalBtn.addEventListener('click', () => {
+  modal.classList.add('active');
+});
+
+closeModalBtn.addEventListener('click', () => {
+  modal.classList.remove('active');
+});
